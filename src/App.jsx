82e55d39c2cbfb44b04f6b993d18c5ca5c683e41
Isai Xuncax <isaixuncax@isais-air.lan>
2023-07-27 15:16:53 -0700

    another change

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,50 @@ import { useState } from 'react'
 import {puppyList} from './data.js'
 import './App.css'
 
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  
+
+function handleClick() {
+  // some logic here
+  
+  
+}
+
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+console.log(featuredPup)
 
   return (
     
-      <div className= "App">
-           {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       } 
+    <div className="App">
+    
+    {puppies.map((puppy) => {
+         
+      return (
+        <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+          {puppy.name}
+        </p>
+      );
+    })}
+      {featPupId && (
+           <div>
+             <h2>{featuredPup.name}</h2>
+             <ul>
+               <li>Age: {featuredPup.age}</li>
+               <li>Email: {featuredPup.email}</li>
+             </ul>
+           </div>
+      )}
 
-      </div>
- 
-   
-  )
+
+  </div>
+);
 }
+
+
+
+  
 
 export default App
